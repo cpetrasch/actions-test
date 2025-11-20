93dc36f9273f87c215f61c4a52bd0ffbc782beee
cpetrasch <66477716+cpetrasch@users.noreply.github.com>
2025-11-20 15:29:24 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,21 @@
-console.log("Hello from GitHub!");
+const url = "https://berufemap.de/verify_events/input.json"; // anpassen!
+
+async function run() {
+    console.log("Rufe Strato-Server auf:", url);
+
+    const res = await fetch(url);
+    if (!res.ok) {
+        console.error("Fehler beim Abruf:", res.status, res.statusText);
+        process.exit(1);
+    }
+
+    const data = await res.json();
+    console.log("Antwort vom Strato-Server:");
+    console.log(data);
+}
+
+run().catch(err => {
+    console.error("Unerwarteter Fehler:", err);
+    process.exit(1);
+});
+
